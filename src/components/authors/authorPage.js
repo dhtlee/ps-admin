b1f8a9b41be8d944fe2cf20489e2685a98d5105d
dhtlee <dhtlee@rocketmail.com>
2017-05-03 13:51:34 -0400

    add dispatcher, actions, and store implementations

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -2,20 +2,14 @@
 
 var React = require('react');
 var Link = require('react-router').Link;
-var AuthorApi = require('../../api/authorApi');
+var AuthorStore = require('../../stores/authorStore');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
   getInitialState: function() {
     return {
-      authors: []
+      authors: AuthorStore.getAllAuthors()
     };
-  },
-
-  componentDidMount: function() {
-    if (this.isMounted()) {
-      this.setState({ authors: AuthorApi.getAllAuthors()});
-    }
   },
 
   render: function() {
